05859078a01b3036d6e95168757af26c6f5deb0c
Harpittt <130437246+Harpittt@users.noreply.github.com>
2023-04-11 21:58:45 +0530

    Update IEEE project.py

--- IEEE project.py
+++ IEEE project.py
@@ -4,37 +4,37 @@ from sklearn.preprocessing import StandardScaler
 import tensorflow as tf
 from tensorflow import keras
 
-# Load the data
+
 data = pd.read_csv("https://github.com/Harpittt/IEEE-project/blob/cc8b72bbf736129fd75e46dac85143fefd0e2e51/Book1.csv")
 
-# Split the data into features and classes
+
 features = data.iloc[:, :12]
 classes = data.iloc[:, 12:]
 
-# Convert the classes to one-hot encoded format
+
 classes = pd.get_dummies(classes)
 
-# Split the data into training and testing sets
+
 X_train, X_test, y_train, y_test = train_test_split(features, classes, test_size=0.2)
 
-# Normalize the feature data
+
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# Define the model architecture
+
 model = keras.Sequential([
     keras.layers.Dense(64, activation='relu', input_shape=(12,)),
     keras.layers.Dense(32, activation='relu'),
     keras.layers.Dense(85, activation='softmax')
 ])
 
-# Compile the model
+
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Train the model
+
 model.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.2)
 
-# Evaluate the model
+
 test_loss, test_acc = model.evaluate(X_test, y_test)
 print('Test accuracy:', test_acc)
